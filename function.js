1043286aca6f39c388ce7d8c3fa9f949592d99f4
michalgealdor224 <nhnh224@gmail.com>
2022-11-17 12:05:34 +0200

    new1

--- function.js
+++ function.js
@@ -4,39 +4,60 @@ function choseWord(){
     chosenWord =words[(Math.random() *19).toFixed()]
     document.getElementById("chosen word").innerText= chosenWord;
 }
-
+id0 = "0"
+id1 = "1"
+id2 = "2"
+id3 = "3"
+id4 = "4"
 function lettersToTextBox(string) {
-     box1 = document.getElementById("0").innerText;
-     box2 = document.getElementById("1").innerText;
-     box3 = document.getElementById("2").innerText;
-     box4 = document.getElementById("3").innerText;
-     box5 = document.getElementById("4").innerText;
+    box1 = document.getElementById(id0).innerText;
+    box2 = document.getElementById(id1).innerText;
+    box3 = document.getElementById(id2).innerText;
+    box4 = document.getElementById(id3).innerText;
+    box5 = document.getElementById(id4).innerText;
     if (box1 === "") {
-        document.getElementById("0").innerText = string;
-        lastLetter = "0"
+        document.getElementById(id0).innerText = string;
+        lastLetter = id0
     } else if (box2 === "") {
-        document.getElementById("1").innerText = string;
-        lastLetter = "1"
+        document.getElementById(id1).innerText = string;
+        lastLetter = id1
     } else if (box3 === "") {
-        document.getElementById("2").innerText = string;
-        lastLetter = "2"
+        document.getElementById(id2).innerText = string;
+        lastLetter = id2
     } else if (box4 === "") {
-        document.getElementById("3").innerText = string;
-        lastLetter = "3"
+        document.getElementById(id3).innerText = string;
+        lastLetter = id3
     } else if (box5 === "") {
-        document.getElementById("4").innerText = string;
-        lastLetter = "4"
+        document.getElementById(id4).innerText = string;
+        box5 =string
+        lastLetter = id4
     }
 
 }
+function addFive(stringId){
+    stringId = Number(stringId)
+    stringId = stringId + 5
+    stringId = stringId.toString()
+    return stringId
+}
+
 
 function main () {
-    // let userInput1 = document.getElementById("11").innerText;
-    // let userInput2 = document.getElementById("12").innerText;
-    // let userInput3 = document.getElementById("13").innerText;
-    // let userInput4 = document.getElementById("14").innerText;
-     let userInput5 = document.getElementById("4").innerText;
-    let wordInput = box1 + box2 + box3 + box4 + userInput5
+
+
+    if (lastLetter === id4) {
+        id0=addFive(id0)
+        id1=addFive(id1)
+        id2=addFive(id2)
+        id3=addFive(id3)
+        id4=addFive(id4)
+    }
+    if (box5 === "") {
+        alert("return")
+        return;
+    }
+
+    let wordInput = box1 + box2 + box3 + box4 + box5;
     alert(wordInput)
     if (chosenWord === wordInput) {
         alert("very good")
@@ -91,7 +112,6 @@ function toDelete () {
         return ifTrue
     }
 
-
     function isExistLetter (string,char) {
         let ifExist = false
         for (i = 0; i < string.length; i++) {
@@ -101,5 +121,8 @@ function toDelete () {
         }
         return ifExist;
     }
+
+
+
 
 
